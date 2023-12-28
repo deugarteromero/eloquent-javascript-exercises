c2753b8dc419cf624a0a4a9d8b64d12d87be305e
deugarteromero <33359642+deugarteromero@users.noreply.github.com>
2023-12-27 20:08:36 -0800

    Add Reverse List to Array

--- exercises/4-chapter/arrayToList.js
+++ exercises/4-chapter/arrayToList.js
@@ -1,25 +1,37 @@
-function listToArray(array){
+function arrayToList(array){
     let nestedObjects = {};
     let tempObjects = {};
+
     for(let i = array.length - 1; i >= 0; i--){
         tempObjects.value = array[i];
-        if(i === array.length - 1){
-            tempObjects.rest = null;
-            nestedObjects = tempObjects;
-            tempObjects = {};
-            // console.log(nestedObjects, tempObjects);
-        } else {
-            console.log(nestedObjects);
-            tempObjects.rest = nestedObjects;
-            nestedObjects = tempObjects;
-            tempObjects = {};
-            // console.log(nestedObjects, tempObjects);
-        };
-        // console.log(i);
+        i === array.length - 1 ? tempObjects.rest = null : tempObjects.rest = nestedObjects;
+        nestedObjects = tempObjects;
+        tempObjects = {};
     };
-    console.log(nestedObjects);
+
+    // console.log(nestedObjects);
+    return nestedObjects;
+};
+
+function listToArray(list, array = []){
+    for(let key in list){
+        if(list.hasOwnProperty(key)){
+            if(typeof list[key] === "object" && list[key] !== null){
+                //Call Again for Next Nested Object
+                listToArray(list[key], array);
+            } else if(key === "value"){
+                array.push(list[key]);
+            };
+        };
+    };
+
+    // console.log(array); Logs 3 Times, Must be Outside of Function
+    return array;
 };
 
 //TESTS
-listToArray([1, 2, 3]); //Converts to Nested Objects
+arrayToList([1, 2, 3]); //Converts to Nested Objects
                         //Result: {value: 1, rest: {value: 2, rest: {value: 3, rest: null}}}
+listToArray(arrayToList([1, 2, 3])); //Converts Back to Array
+                                     //Result: [1, 2, 3];
+console.log(listToArray(arrayToList([1, 2, 3])));
